57e2430e47d4ac7622a84ae877a7f62832238351
Noah <nordqvist.gaming@gmail.com>
2019-04-11 10:22:57 +0200

    PoC Nitpicks 1

---
+++ UnityProject/TheFens/Assets/Scripts/MapManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+
+    public GameObject nodePrefab;
+    public GameObject nodePanel;
+
+    public GameObject[] nodes;
+
+    void GenerateNodes() {
+        nodes = new GameObject[50];
+
+        foreach (GameObject node in nodes) {
+            Instantiate(nodePrefab, nodePanel.transform);
+        }
+    }
+}
---
+++ UnityProject/TheFens/Assets/Scripts/Node.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public GameObject previous;
+
+    public GameObject[] choices;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        
+    }
+}
